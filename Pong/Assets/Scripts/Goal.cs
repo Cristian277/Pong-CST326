6e026d4a959a6230a83c222a55df9638d359aa71
Cristian <carredondo@csumb.edu>
2021-02-06 20:04:49 -0800

    Moved Package to new Permanent Project
    Had another project to work on the project without worrying about messing up, but wanted to start a new one to get running with git so I transferred the package, which is why a lot of the work is done already.

---
+++ Pong/Assets/Scripts/Goal.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    public bool isPlayer1Goal;
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Ball"))
+        {
+            if (isPlayer1Goal)
+            {
+                Debug.Log("Player 2 Scored");
+                GameObject.Find("Manager").GetComponent<Manager>().Player2Scored();
+            }
+            else
+            {
+                Debug.Log("Player 1 Scored");
+                GameObject.Find("Manager").GetComponent<Manager>().Player1Scored();
+            }
+
+        }
+    }
+}
---
+++ Pong/Assets/Scripts/Paddle.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    public bool isPlayer1;
+    public float speed = 5f;
+    public Vector3 startPosition;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        startPosition = gameObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPlayer1)
+        {
+            transform.Translate(0f, Input.GetAxisRaw("Vertical") * speed * Time.deltaTime,0f);
+        }
+        else
+        {
+            transform.Translate(0f, Input.GetAxisRaw("Vertical2") * speed * Time.deltaTime, 0f);
+        }
+    }
+
+    public void Reset()
+    {
+        transform.position = startPosition;
+    }
+}
+
